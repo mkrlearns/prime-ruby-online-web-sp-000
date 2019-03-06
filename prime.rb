fcaf4a2e476f7a5f54786dbf9c447e1daab20282
ryan m <contact@ryanmeek.com>
2019-03-06 20:12:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 def prime?(int)
   return (2..int - 1).all?{|x| int % x != 0} if int > 2
   return true if int == 2
   false
+=======
+def prime?(int)
+  if int > 1
+      if int.prime?
+        return true
+      else
+        return false
+      end
+  end
+>>>>>>> a5b3b6952df21f112ac7a0103c4c5aee7225f591
 end
